714672f72168939b705bb2f52f72e6fda1df78d6
Richard Law <richard.m.law@gmail.com>
2017-09-07 23:14:30 +1200

    improvements to exercise 9

--- chapter12-pointers-and-arrays/exercises/ex9.c
+++ chapter12-pointers-and-arrays/exercises/ex9.c
@@ -1,33 +1,32 @@
 // Checks if a message is a palindrome
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <ctype.h>
 
 #define MSG_LEN 20 // Maximum length of message
+bool is_palindrome(char *a, int n);
 
 int main(void){
   char msg[MSG_LEN], ch;
-  char *p = msg;
-  char *q = p;
+  int i = 0;
   printf("Enter a message: ");
-  while (p < msg + MSG_LEN) {
-    ch = getchar();
-    if (ch == '\n') {
-      q--;
-      break;
-    }
+  while (i < MSG_LEN && (ch = getchar()) != '\n') {
     if (isalpha(ch)) {
-      *q++ = tolower(ch);
+      msg[i++] = tolower(ch);
     }
   }
-  while (p <= q) {
-    if (!(*p == *q)) {
-      printf("Not a palindrome\n");
-      return 0;
-    }
-    p++;
-    q--;
-  }
-  printf("Palindrome\n");
+  if (is_palindrome(msg, i - 1))
+    printf("Palindrome\n");
+  else
+    printf("Not a palindrome\n");
   return 0;
+}
+
+bool is_palindrome(char *a, int n) {
+  char *left, *right;
+  for (left = a, right = a + n; left <= right; left++, right--)
+    if (*left != *right)
+      return false;
+  return true;
 }
